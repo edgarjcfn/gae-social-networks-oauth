d137581874eb5d2d49c2efe399701b0d151fe479
Edgar Neto <edgar.figueiredo@gmail.com>
2013-05-22 00:07:28 +0200

    Structured twitter.py. Still getting Failure (to validate oauth signature and token) from Twitter's side. Investigate tomorrow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from facebook import Facebook
 from twitter import Twitter
 
 application = webapp2.WSGIApplication([
-    ('/', Home),
-    ('/facebook_auth', Facebook),
-    ('/twitter_auth', Twitter),
+	webapp2.Route(r'/', handler=Home, name='home'),
+	webapp2.Route(r'/auth/facebook', handler=Facebook, name='facebook'),
+	webapp2.Route(r'/auth/twitter', handler=Twitter, name='twitter'),
 ], debug=True) 
--- twitter.py
+++ twitter.py
@@ -1,7 +1,27 @@
 import webapp2
+import urllib
 
+
+REQUEST_TOKEN_URL = 'http://www.twitter.com/oauth/request_token'
 class Twitter(webapp2.RequestHandler):
+
+    request_token = None;
+
     def get(self):
-        self.response.write('//TODO: Twitter') 
+        if not self.has_request_token():
+        # Initial state: obtain request token
+            self.obtain_request_token()
+        else:
+        # Has request token
+            self.redirect_to_auth_page()
 
+    def has_request_token(self):
+        return self.request_token != None
 
+    def obtain_request_token(self):
+        request_params = {
+            'oauth_callback' : webapp2.uri_for('twitter', _full=True),
+        }
+        self.redirect("%s?%s" % (REQUEST_TOKEN_URL, urllib.urlencode(request_params)))
+    def redirect_to_auth_page(self):
+        self.response.write('Redirecting to auth page!')
